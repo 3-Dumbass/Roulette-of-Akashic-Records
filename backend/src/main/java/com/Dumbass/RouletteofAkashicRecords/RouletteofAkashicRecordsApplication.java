0ea0b37e9b88678ad9d348920509a38896871cab
LEE026 <lhs456852@gmail.com>
2024-05-12 20:39:26 +0900

    spring 초기세팅

---
+++ backend/src/main/java/com/Dumbass/RouletteofAkashicRecords/RouletteofAkashicRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.Dumbass.RouletteofAkashicRecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteofAkashicRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteofAkashicRecordsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Dumbass/RouletteofAkashicRecords/RouletteofAkashicRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dumbass.RouletteofAkashicRecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteofAkashicRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
