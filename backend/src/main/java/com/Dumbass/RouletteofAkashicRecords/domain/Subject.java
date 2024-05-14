ffed7aa8767969ac82ceccc32cf3fd4927761a7e
LEE026 <lhs456852@gmail.com>
2024-05-14 16:39:55 +0900

    주제 엔티티 제작

---
+++ backend/src/main/java/com/Dumbass/RouletteofAkashicRecords/domain/Subject.java
@@ -0,0 +1,28 @@
+package com.Dumbass.RouletteofAkashicRecords.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Subject {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String subject;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+}
