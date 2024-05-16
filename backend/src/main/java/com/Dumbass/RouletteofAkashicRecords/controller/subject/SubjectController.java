cd6d26b7817e66302f0db6f21c5b203c1be475fb
LEE026 <lhs456852@gmail.com>
2024-05-16 16:32:51 +0900

    컨트롤러에 api 설명 추가

--- backend/src/main/java/com/Dumbass/RouletteofAkashicRecords/controller/subject/SubjectController.java
+++ backend/src/main/java/com/Dumbass/RouletteofAkashicRecords/controller/subject/SubjectController.java
@@ -2,6 +2,8 @@ package com.Dumbass.RouletteofAkashicRecords.controller.subject;
 
 import com.Dumbass.RouletteofAkashicRecords.controller.subject.dto.SubjectSaveDto;
 import com.Dumbass.RouletteofAkashicRecords.service.SubjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/subject")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "subject api", description = "주제와 관련된 api")
 public class SubjectController {
 
     private final SubjectService subjectService;
 
     @PostMapping("/save")
+    @Operation(summary = "주제 저장", description = "사용자가 입력란 주제를 저장")
     public ResponseEntity<Long> saveSubject(@RequestBody @Valid SubjectSaveDto subjectSaveDto){
         log.info("주제 입력: "+subjectSaveDto.getContent());
         Long id = subjectService.saveSubject(subjectSaveDto.getContent());
