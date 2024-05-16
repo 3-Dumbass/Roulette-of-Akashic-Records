286982a2dc9e263a1a4d1e1126ee68d8f9a4fc91
LEE026 <lhs456852@gmail.com>
2024-05-16 14:41:46 +0900

    SubjectRepository 인터페이스 제작

---
+++ backend/src/main/java/com/Dumbass/RouletteofAkashicRecords/repository/SubjectRepository.java
@@ -0,0 +1,26 @@
+package com.Dumbass.RouletteofAkashicRecords.repository;
+
+import com.Dumbass.RouletteofAkashicRecords.domain.Subject;
+
+public interface SubjectRepository {
+
+    /**
+     * 주제 저장
+     * @param subject 주제 객체
+     * @return 주제 객체의 id
+     */
+    Long save(Subject subject);
+
+    /**
+     * id 기반으로 주제 찾기
+     * @param id 주제 객체의 id
+     * @return 찾아진 주제
+     */
+    Subject findById(Long id);
+
+    /**
+     * id로 주제 삭제
+     * @param id 삭제할 주제의 id
+     */
+    void deleteById(Long id);
+}
