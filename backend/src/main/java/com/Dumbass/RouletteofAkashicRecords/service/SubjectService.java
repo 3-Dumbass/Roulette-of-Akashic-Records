0df612d8c7f5b9d53ec669cd22e3cbb5d1f4a5fc
LEE026 <lhs456852@gmail.com>
2024-05-16 14:50:21 +0900

    SubjectService 제작 - 저장 부분만 제작

---
+++ backend/src/main/java/com/Dumbass/RouletteofAkashicRecords/service/SubjectService.java
@@ -0,0 +1,21 @@
+package com.Dumbass.RouletteofAkashicRecords.service;
+
+import com.Dumbass.RouletteofAkashicRecords.domain.Subject;
+import com.Dumbass.RouletteofAkashicRecords.repository.SubjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SubjectService {
+
+    private final SubjectRepository subjectRepository;
+
+    public Long saveSubject(String content){
+        Subject subject = new Subject(content);
+        subjectRepository.save(subject);
+        return subject.getId();
+    }
+}
