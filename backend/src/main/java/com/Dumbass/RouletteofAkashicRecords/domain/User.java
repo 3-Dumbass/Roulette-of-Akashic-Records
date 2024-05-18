d86122c43bd29e61c55a197488ba467ec32abb78
LEE026 <lhs456852@gmail.com>
2024-05-18 16:51:31 +0900

    사용자 엔티티 추가

---
+++ backend/src/main/java/com/Dumbass/RouletteofAkashicRecords/domain/User.java
@@ -0,0 +1,26 @@
+package com.Dumbass.RouletteofAkashicRecords.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public User(String name) {
+        if(name.isBlank()){
+            throw new IllegalArgumentException("이름이 비어있거나 공백으로만 이루어져 있습니다");
+        }
+        this.name = name;
+    }
+}
