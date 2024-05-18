02554441fd42afaa7f392f52d792bd877f7e3790
LEE026 <lhs456852@gmail.com>
2024-05-18 16:30:43 +0900

    주제 삭제 컨트롤러 제작

--- backend/src/main/java/com/Dumbass/RouletteofAkashicRecords/controller/subject/SubjectController.java
+++ backend/src/main/java/com/Dumbass/RouletteofAkashicRecords/controller/subject/SubjectController.java
@@ -1,5 +1,6 @@
 package com.Dumbass.RouletteofAkashicRecords.controller.subject;
 
+import com.Dumbass.RouletteofAkashicRecords.controller.subject.dto.SubjectRemoveRequest;
 import com.Dumbass.RouletteofAkashicRecords.controller.subject.dto.SubjectSaveDto;
 import com.Dumbass.RouletteofAkashicRecords.service.SubjectService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/subject")
@@ -29,5 +27,13 @@ public class SubjectController {
         log.info("주제 입력: "+subjectSaveDto.getContent());
         Long id = subjectService.saveSubject(subjectSaveDto.getContent());
         return new ResponseEntity<>(id, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/remove")
+    @Operation(summary = "주제 삭제", description = "사용자가 요구한 주제를 삭제")
+    public ResponseEntity<Void> removeSubject(@RequestBody @Valid SubjectRemoveRequest subjectRemoveRequest){
+        log.info("주제 삭제: "+subjectRemoveRequest.getSubjectId());
+        subjectService.deleteSubject(subjectRemoveRequest.getSubjectId());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
